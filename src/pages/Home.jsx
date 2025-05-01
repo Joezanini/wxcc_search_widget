76a8e9e7deb28341a9f26c63528f2d4c23b662a4
jozanini <jozanini@cisco.com>
2025-05-01 15:55:28 -0700

    main app added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Search from '../components/Search.jsx'
+
+function Home() {
+  return (
+    <>
+     <div>WxCC Search</div>
+     <Search />
+    </>
+  )
+}
+
+export default Home
